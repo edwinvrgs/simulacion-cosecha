619582e7d8e6291b79d3b23c771fff928f8ef0fc
Edwin Vargas <onix.edwin18@gmail.com>
2016-12-03 16:12:49 -0400

    Security problems jeje

--- sketchs/NodeSketch/NodeSketch.ino
+++ sketchs/NodeSketch/NodeSketch.ino
@@ -18,49 +18,49 @@ ModbusIP mb;
 
 void setup() {
   Serial.begin(115200);
-  
-  mb.config("Onix", "180196260499");
+
+  mb.config("SSID", "PASSWORD");
   while (WiFi.status() != WL_CONNECTED) { delay(250); }
   Serial.println(WiFi.localIP());
-  
+
   for (int i = 0, j = 0; i < cin_count, j < cout_count; i++, j++) {
     mb.addCoil(in_coils[i]);
     mb.addCoil(out_coils[j]);
   }
-  
+
   for (int i = 0; i < hin_count; i++) {
     mb.addHreg(in_hreg[i]);
   }
-  
+
   ts = millis();
 }
 
 void loop() {
-  
+
    mb.task();
    int data = 0;
-   
+
    if (Serial.available()) {
-    
+
      for (int i = 0; i < cin_count; i++) {
        data = Serial.parseInt();
        mb.Coil(in_coils[i], data);
 
        Serial.println(data);
-     }  
-    
+     }
+
      for (int i = 0; i < hin_count; i++) {
        data = Serial.parseInt();
        mb.Hreg(in_hreg[i], data);
-       
+
        Serial.println(data);
-     }  
-     
-     for (int i = 0; i < cout_count; i++) {  
+     }
+
+     for (int i = 0; i < cout_count; i++) {
        int data = mb.Coil(out_coils[i]);
-       Serial.println(data); 
-       
+       Serial.println(data);
+
        while(Serial.parseInt() != data){}
-     }   
+     }
    }
 }
